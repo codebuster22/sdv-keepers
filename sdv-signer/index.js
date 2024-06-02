5a1d0e2b664d81aae4c7492fa6fc762e6754864f
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-06-02 15:13:57 +0530

    fix: not use default when exporting

--- sdv-signer/index.js
+++ sdv-signer/index.js
@@ -34,4 +34,4 @@ const handler = async (event) => {
     }
 };
 
-export default {handler};
+export {handler};
--- sdv-signer/test.js
+++ sdv-signer/test.js
@@ -4,5 +4,5 @@ import * as exportedHandler from  "./index.js";
 // const newWallet = new ethers.Wallet("0xe006b5a80e09d549b3a5a8c4a7ae5e65f4c33cc747066ea5b0f4a3cf18c471ca");
 // console.log(newWallet.privateKey, newWallet.address);
 
-console.log(await exportedHandler.default.handler({toAddress: "0x9cA70B93CaE5576645F5F069524A9B9c3aef5006"}));
+console.log(await exportedHandler.handler({toAddress: "0x9cA70B93CaE5576645F5F069524A9B9c3aef5006"}));
 // console.log(handler);
